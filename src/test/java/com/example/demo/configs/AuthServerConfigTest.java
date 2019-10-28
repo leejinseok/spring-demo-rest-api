d3e248683fbd77c7443c2051917784b013027f2c
leejinseok <sonaky47@gmail.com>
2019-10-28 12:03:20 +0900

    mockMvc test code

---
+++ src/test/java/com/example/demo/configs/AuthServerConfigTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.configs;
+
+import com.example.demo.accounts.AccountService;
+import com.example.demo.common.AppProperties;
+import com.example.demo.common.BaseController;
+import com.example.demo.common.TestDescription;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class AuthServerConfigTest extends BaseController {
+  @Autowired
+  AccountService accountService;
+
+  @Autowired
+  AppProperties appProperties;
+
+  @Test
+  @TestDescription("인증 토큰을 발급 받는 테스트")
+  public void getAuthToken() throws Exception {
+    this.mockMvc.perform(post("/oauth/token")
+      .with(httpBasic(appProperties.getClientId(), appProperties.getClientSecret()))
+      .param("username", appProperties.getUserUsername())
+      .param("password", appProperties.getUserPassword())
+      .param("grant_type", "password"))
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(jsonPath("access_token").exists());
+  }
+}
